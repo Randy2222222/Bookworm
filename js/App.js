27066429d0c09fed90d9cd7e8e24478e4ac91abe
Randy2222222 <randyjpollitt@yahoo.com>
2026-02-22 02:05:37 -0500

    Create App.js

---
+++ js/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BookMailApp from "./BookMailApp";
+
+ReactDOM.render(<BookMailApp currentUser="user_123" />, document.getElementById("root"));
