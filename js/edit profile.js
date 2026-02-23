8ae1243cbf3db9651f6d25ea28fe59619ddfaec7
Randy2222222 <randyjpollitt@yahoo.com>
2026-02-22 22:54:15 -0500

    Update edit profile.js

--- js/edit profile.js
+++ js/edit profile.js
@@ -1,14 +1,23 @@
 
 document.getElementById('editBtn').addEventListener('click', function() {
-    document.getElementById('name').disabled = false;
-    document.getElementById('email').disabled = false;
+    document.getElementById('Username').disabled = false;
+    document.getElementById('Age').disabled = false;
+    document.getElementById('Career').disabled = false;
+    document.getElementById('Hobbies').disabled = false;
+    document.getElementById('Reading Style').disabled = false;
+    document.getElementById('Bio').disabled= false;
     this.style.display = 'none';
     document.getElementById('saveBtn').style.display = 'inline';
 });
 
 document.getElementById('saveBtn').addEventListener('click', function() {
-    document.getElementById('name').disabled = true;
-    document.getElementById('email').disabled = true;
+    document.getElementById('Username').disabled = true;
+    document.getElementById('Age').disabled = true;
+    document.getElementById('Career').disabled = true;
+    document.getElementById('Hobbies').disabled = true;
+    document.getElementById('Reading Style').disabled = true;
+    document.getElementById('Bio').disabled = true;
+    document.getElementById("myTxtbox").value;
     this.style.display = 'none';
     document.getElementById('editBtn').style.display = 'inline';
 });
