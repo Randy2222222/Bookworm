6d0c345215423afa41e27ae91e4eab605b062a6e
Randy2222222 <randyjpollitt@yahoo.com>
2026-02-24 04:18:57 -0500

    Update editProfile.js

--- js/editProfile.js
+++ js/editProfile.js
@@ -12,11 +12,11 @@ document.getElementById('editBtn').addEventListener('click', function() {
 });
 
 document.getElementById('saveBtn').addEventListener('click', function() {
-    document.getElementById('username').style.display.color = '#FFFFFF'.width.40%.disabled = true;
-    document.getElementById('age').style.display.color = '#FFFFFF'.width.40%.disabled = true;
-    document.getElementById('career').style.display.color = '#FFFFFF'.width.49%.disabled = true;
-    document.getElementById('hobbies').style.display.color = '#FFFFFF'.width.40%.disabled = true;
-    document.getElementById('readingstyle').style.display.color = '#FFFFFF'.width.40%.disabled = true;
+    document.getElementById('username').style.display.color = '#FFFFFF'.disabled = true;
+    document.getElementById('age').style.display.color = '#FFFFFF'.disabled = true;
+    document.getElementById('career').style.display.color = '#FFFFFF'.disabled = true;
+    document.getElementById('hobbies').style.display.color = '#FFFFFF'.disabled = true;
+    document.getElementById('readingstyle').style.display.color = '#FFFFFF'.disabled = true;
     document.getElementById('bio').style.display.color = '#FFFFFF'.disabled = true;
     document.getElementById('webaddress').style.display.color = '#FFFFFF'.disabled = true;
     this.style.display = 'none';
