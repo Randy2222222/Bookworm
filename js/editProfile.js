4c10b6258a1b87ea3b9fad39b2cda7d85b5e7bac
Randy2222222 <randyjpollitt@yahoo.com>
2026-02-25 01:41:48 -0500

    Update editProfile.js

--- js/editProfile.js
+++ js/editProfile.js
@@ -1,24 +1,24 @@
 
 document.getElementById('editBtn').addEventListener('click', function() {
-    document.getElementById('username').readOnly = false;
-    document.getElementById('age').readOnly = false;
-    document.getElementById('career').readOnly = false;
-    document.getElementById('hobbies').readOnly = false;
-    document.getElementById('readingstyle').readInly = false;
-    document.getElementById('bio').readOnly = false;
-    document.getElementById('webaddress').readOnly = false;
+    document.getElementById('username').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('age').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('career').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('hobbies').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('readingstyle').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('bio').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('webaddress').style.color = '#FFFFFF'.disabled = true;
     this.style.display = 'none';
     document.getElementById('saveBtn').style.display = 'inline';
 });
 
 document.getElementById('saveBtn').addEventListener('click', function() {
-    document.getElementById('username').style.color = '#FFFFFF'.readOnly = true;
-    document.getElementById('age').style.color = '#FFFFFF'.readOnly = true;
-    document.getElementById('career').style.color = '#FFFFFF'.readOnly = true;
-    document.getElementById('hobbies').style.color = '#FFFFFF'.readOnly = true;
-    document.getElementById('readingstyle').style.color = '#FFFFFF'.readOnly = true;
-    document.getElementById('bio').style.color = '#FFFFFF'.readOnly = true;
-    document.getElementById('webaddress').readOnly = true;
+    document.getElementById('username').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('age').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('career').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('hobbies').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('readingstyle').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('bio').style.color = '#FFFFFF'.disabled = true;
+    document.getElementById('webaddress').disabled = true;
     this.style.display = 'none';
     document.getElementById('editBtn').style.display = 'inline';
 });
