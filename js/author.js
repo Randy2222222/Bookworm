a1aab0fd42d0e4e2c153022679a721486547da1b
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-02 03:04:52 -0500

    Create author.js

---
+++ js/author.js
@@ -0,0 +1,14 @@
+
+//document.getElementById('editBtn').addEventListener('click', function() {
+
+document.getElementById('image').disabled = false;
+    this.style.display = 'none';
+  document.getElementById('saveBtn').style.display = 'inline';
+//}); 
+
+
+document.getElementById('saveBtn').addEventListener('click', function() {
+
+    this.style.display = 'none';
+    document.getElementById('editBtn').style.display = 'inline';
+});
