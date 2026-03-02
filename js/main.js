19cbead63d7e21e69207130f39fee494082ae075
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-02 00:56:25 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 //document.getElementById('editBtn').addEventListener('click', function() {
 (function() {
     // Code to run immediately-got rid of button temporarly-commented out code
+      document.getElementById('post').disabled = false;
+    document.getElementById('author').disabled = false;
+    document.getElementById('image').disabled = false;
+    this.style.display = 'none';
+    document.getElementById('saveBtn').style.display = 'inline';
+//}); 
+})();
