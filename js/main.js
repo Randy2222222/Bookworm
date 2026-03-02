b6760038754a118657d8ed44475b5dee5eaae947
Randy2222222 <randyjpollitt@yahoo.com>
2026-03-02 13:25:22 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 (function() {
     // Code to run immediately-got rid of button temporarly-commented out code
       document.getElementById('post').disabled = false;
-    document.getElementById('author').disabled = false;
-    document.getElementById('image').disabled = false;
+ //   document.getElementById('author').disabled = false;
+//    document.getElementById('image').disabled = false;
     this.style.display = 'none';
     document.getElementById('saveBtn').style.display = 'inline';
 //}); 
@@ -11,8 +11,8 @@
 
 document.getElementById('saveBtn').addEventListener('click', function() {
 document.getElementById('post').style.color = '#FFFFFF'.disabled = true;
-    document.getElementById('author').style.color = '#FFFFFF'.disabled = true;
-    document.getElementById('image').disabled = true;
+ //   document.getElementById('author').style.color = '#FFFFFF'.disabled = true;
+ //   document.getElementById('image').disabled = true;
     this.style.display = 'none';
     document.getElementById('editBtn').style.display = 'inline';
 });
